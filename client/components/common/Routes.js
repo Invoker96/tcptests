93195440899466508a006275f5bd50e7cb2d758a
Shubham Bhardwaj <shubham1089@gmail.com>
2018-07-07 21:48:35 +0530

    Top Posts

--- client/components/common/Routes.js
+++ client/components/common/Routes.js
@@ -2,12 +2,14 @@ import React from "react";
 import App from "./App";
 import Single from "../single/Single";
 import PhotoGrid from "../gallery/grid/PhotoGrid";
+import Home from "../gallery/Home";
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
 const Routes = () => (
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <IndexRoute component={PhotoGrid} />
+        <IndexRoute component={Home} />
+        <Route path="/view" component={PhotoGrid} />
         <Route path="/view/:postId" component={Single} />
       </Route>
     </Router>
