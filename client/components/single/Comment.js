6f069fa6560e68313e590e7d770f24edaa6011e3
Shubham Bhardwaj <shubham1089@gmail.com>
2018-07-08 20:46:22 +0530

    Feature/shubham (#7)
    * top comments added
    
    * Top Posts
    
    * Add files via upload (#2)
    
    * Dragoonblade patch 2 (#4)
    
    * Feature/shubham (#3)
    
    * top comments added
    
    * Top Posts
    
    * Add files via upload (#2)
    
    * Add files via upload
    
    * Add files via upload (#5)
    
    * removing logic from reducers
    
    * Add files via upload

--- client/components/single/Comment.js
+++ client/components/single/Comment.js
@@ -1,25 +1,31 @@
 import React from "react";
 
-const Comment = ({ postId, comment, i, removeComment, top }) => {
-  return (
-    <div className="comment" key={i}>
-      <p>
-        <strong>{comment.user}</strong>
-        {comment.text}
-        <button
-          className="remove-comment"
-          onClick={() => removeComment(postId, i)}
-        >
-          &times;
-        </button>
-        <button
-          className="edit-comment"
-          onClick={() => editComment(postId, i)}
-        >
-          &#x270f;
-        </button>
-      </p>
-    </div>
-  );
-};
+const Comment = ({
+  postId,
+  comment,
+  i,
+  removeComment,
+  editComment,
+  top,
+  handleUpdate
+}) => (
+  <div className="comment" key={i}>
+    <p>
+      <strong name="user">{comment.user}</strong>
+      <span name="comment">{comment.text}</span>
+      <button
+        className="remove-comment"
+        onClick={() => removeComment(postId, i)}
+      >
+        &times;
+      </button>
+      <button
+        className="edit-comment"
+        onClick={e => handleUpdate(e, postId, i)}
+      >
+        &#x270f;
+      </button>
+    </p>
+  </div>
+);
 export default Comment;
